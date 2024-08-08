12eb5bae195f9fcd092b8590337b25239798f6d2
JI YEON <jykim9280@gmail.com>
2024-08-08 17:53:08 +0900

    ML 02

---
+++ 05data_scraping/naver_api_info.py
@@ -0,0 +1,9 @@
+client_id = "VfCSVYpBxY6EEAWW1zcr" # 네이버 api에 접속 가능한 id 
+client_secret = "bTXn2nJ3Ig"       # 네이버 api에 접속 가능한 pw 
+
+db="mysql"
+dbtype="pymysql"
+id="root"
+pw="1234"
+host="127.0.0.1:3306"
+news_database="fintech_news"
